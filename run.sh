b0d302b7ebea70883a733030df285788c9c58af7
anujedits23-design <anujedits23@gmail.com>
2026-04-23 12:11:50 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,17 @@
 echo "Cloning Repo...."
-if [ -z $BRANCH ]
+
+if [ -z "$BRANCH" ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/anujedits23-design/Advance-Content-Saver-Bot anujedits23-design/Advance-Content-Saver-Bot 
+  git clone https://github.com/anujedits23-design/Advance-Content-Saver-Bot
+  cd Advance-Content-Saver-Bot
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/anujedits23-design/Advance-Content-Saver-Bot -b $BRANCH /Advance-Content-Saver-Bot
+  git clone -b $BRANCH https://github.com/anujedits23-design/Advance-Content-Saver-Bot
+  cd Advance-Content-Saver-Bot
 fi
-cd anujedits23-design/Advance-Content-Saver-Bot 
+
 pip3 install -U -r requirements.txt
+
 echo "Starting Bot...."
 python3 main.py
