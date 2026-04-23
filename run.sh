ef63da29425b2ec2d1acf89a499172940f0fd17c
anujedits23-design <anujedits23@gmail.com>
2026-04-23 12:03:32 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/SadKidBGMZ/fwdbot SadKidBGMZ/fwdbot 
+  git clone https://github.com/anujedits23-design/Advance-Content-Saver-Bot anujedits23-design/Advance-Content-Saver-Bot 
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/SadKidBGMZ/fwdbot -b $BRANCH /fwdbot
+  git clone https://github.com/anujedits23-design/Advance-Content-Saver-Bot -b $BRANCH /fwdbot
 fi
-cd SadKidBGMZ/fwdbot 
+cd anujedits23-design/Advance-Content-Saver-Bot 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
